b534b3ee6cc9cf59f3cf65cda0775594b3dde1c1
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-05-31 01:17:05 -0300

    chore: config cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,13 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
+app.register(cors, {
+  origin: true, // todas URLs poderão acessar o back-end
+//origin: ['http://localhost:3000'] para especificar rotas
+})
 app.register(memoriesRoutes)
 
 app
