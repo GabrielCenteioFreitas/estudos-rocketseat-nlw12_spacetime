ab16db4b20f69309c1eac59659b743bb0349e196
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-06-02 14:56:33 -0300

    chore(web): change API route

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -4,7 +4,7 @@ let baseURL = ''
 if (process.env.NODE_ENV === 'development') {
   baseURL = 'http://localhost:3333'
 } else if (process.env.NODE_ENV === 'production') {
-  baseURL = 'https://rocketseat-nlw12-backend.onrender.com'
+  baseURL = 'https://rocketseat-nlw12-backend.vercel.app'
 }
 
 export const api = axios.create({
